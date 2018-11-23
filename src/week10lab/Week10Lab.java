2bed045385256d4bfb69897a4f9407c814e61cc4
Calum Schmidtke <200366773@student.georgianc.on.ca>
2018-11-22 23:43:49 -0500

    Initial upload

---
+++ src/week10lab/Week10Lab.java
@@ -0,0 +1,21 @@
+package week10lab;
+
+/**
+ *
+ * @author Calum
+ */
+public class Week10Lab {
+
+    public static void main(String[] args) {
+        
+        Book Book1 = new Book("Tears in Rain", "Saad Maan", 4.50);
+        
+        Book Book2 = new Book("Bacon and Us", "Chris P. Bacon", 10.00);
+        
+        Book Book3 = new Book("Superhero's for Dummies", "Bart Mahn", 12.25);
+        
+        Book Book4 = new Book("It can be hard to concentr", "Adam Hd", 5.00);
+        
+    }
+    
+}
